ac659e52abd00c69b08d724cd1d603bb8f31fd52
GoZaddy <faruqyusuff437@gmail.com>
2021-02-06 12:58:29 -0600

    SMALL CHANGES

--- custom_logging.py
+++ custom_logging.py
@@ -9,14 +9,13 @@ class Logger:
             dt = datetime.now()
             f.write('[{0}] LOG FILE OPENED\n'.format(dt.ctime()))
 
-
     def write(self, log_string):
         # write log to file
         with open(self.file_path, 'a') as f:
             dt = datetime.now()
             f.write('[{0}] {1}\n'.format(dt.ctime(), log_string))
 
-    def read(self) -> list[str]:
+    def read(self):
         # read logs from file
         with open(self.file_path, 'r') as f:
             content = f.readlines()
