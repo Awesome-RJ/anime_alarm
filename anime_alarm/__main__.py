f4fb4a2d08de0ec0c49b357de1a52e6dabd20188
GoZaddy <faruqyusuff437@gmail.com>
2021-02-16 12:49:01 -0600

    changes

--- anime_alarm/__main__.py
+++ anime_alarm/__main__.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     print('started')
     updater.start_webhook(
         listen='0.0.0.0',
-        key='private.key',
-        cert='cert.pem',
+        key='./private.key',
+        cert='./cert.pem',
         url_path=os.getenv('TELEGRAM_TOKEN'),
         webhook_url=os.getenv('TELEGRAM_BOT_URL') + os.getenv('TELEGRAM_TOKEN')
     )
